559400645b609f53e9e094ed722b7a1d0d5bb87a
3wdian0 <13599488868@163.com>
2019-11-08 17:16:16 +0800

    备忘录法

--- 15-动态规划/15.1-钢条切割/cutRod.cc
+++ 15-动态规划/15.1-钢条切割/cutRod.cc
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-
+// 法一
 int cutRod(vector<int> &p, int n){
     if(n==0)
         return 0;
@@ -18,6 +18,31 @@ int cutRod(vector<int> &p, int n){
 
 }
 
+// auxiliary
+int memoizedCutRodAux(vector<int> &p, int n , vector<int> &r);
+
+// main procedure
+int memoizedCutRod(vector<int> &p, int n){
+    vector<int> r;
+    for(int i = 0; i < p.size(); i++){
+        r.push_back(INT_MIN);
+    }
+    return memoizedCutRodAux(p,n,r);
+}
+
+int memoizedCutRodAux(vector<int> &p, int n , vector<int> &r){
+    if(n==0)
+        return 0;
+    if(r[n-1]!=INT_MIN)
+        return r[n-1];
+    int q = INT_MIN;
+    for(int i = 1; i <= n; i++){
+        q = max(q, p[i-1]+memoizedCutRodAux(p,n - i,r) );
+    }
+    r[n-1] = q;
+    return r[n-1];
+}
+
 int main(int argc, char *argv []){
     vector<int> p ;
     /*
@@ -26,6 +51,7 @@ int main(int argc, char *argv []){
      */
     p.push_back(1); p.push_back(5); p.push_back(8); p.push_back(9); p.push_back(10);
     p.push_back(17);p.push_back(17);p.push_back(20);p.push_back(24);p.push_back(30);
-    cout<< cutRod(p, 9) <<endl;
+    cout<<"法一"<< cutRod(p, 9) <<endl;
+    cout<<"法二"<< memoizedCutRod(p, 9) <<endl;
     return 0;
 }
