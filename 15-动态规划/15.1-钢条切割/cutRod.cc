1c15284ef714609a3bcf12a995edb0fedfafe303
3wdian0 <13599488868@163.com>
2019-11-09 12:12:44 +0800

    自底向上参考《算法导论》

--- 15-动态规划/15.1-钢条切割/cutRod.cc
+++ 15-动态规划/15.1-钢条切割/cutRod.cc
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-// 法一
+// 法一：递归
 int cutRod(vector<int> &p, int n){
     if(n==0)
         return 0;
@@ -18,31 +18,45 @@ int cutRod(vector<int> &p, int n){
 
 }
 
-// auxiliary
+// 法二：备忘录法
 int memoizedCutRodAux(vector<int> &p, int n , vector<int> &r);
-
 // main procedure
 int memoizedCutRod(vector<int> &p, int n){
     vector<int> r;
-    for(int i = 0; i < p.size(); i++){
+    for(int i = 0; i <= p.size(); i++){
         r.push_back(INT_MIN);
     }
     return memoizedCutRodAux(p,n,r);
 }
-
+// auxiliary
 int memoizedCutRodAux(vector<int> &p, int n , vector<int> &r){
     if(n==0)
         return 0;
-    if(r[n-1]!=INT_MIN)
-        return r[n-1];
+    if(r[n]!=INT_MIN)
+        return r[n];
     int q = INT_MIN;
     for(int i = 1; i <= n; i++){
         q = max(q, p[i-1]+memoizedCutRodAux(p,n - i,r) );
     }
-    r[n-1] = q;
-    return r[n-1];
+    r[n] = q;
+    return r[n];
 }
 
+int bottomUpCutRod(vector<int> &p, int n){
+    vector<int> r;
+    for(int i = 0; i <= p.size(); i++){
+        r.push_back(0);
+    }
+    for(int j=1; j <=n; j++){
+        int q = INT_MIN;
+        for(int i=1; i<=j; i++){
+            q = max(q,p[i-1]+r[j-i])
+        }
+        r[j] = q;
+    }
+
+    return r[n];
+}
 int main(int argc, char *argv []){
     vector<int> p ;
     /*
@@ -51,7 +65,19 @@ int main(int argc, char *argv []){
      */
     p.push_back(1); p.push_back(5); p.push_back(8); p.push_back(9); p.push_back(10);
     p.push_back(17);p.push_back(17);p.push_back(20);p.push_back(24);p.push_back(30);
+    cout<<"法一"<< cutRod(p, 1) <<endl;
+    cout<<"法一"<< cutRod(p, 2) <<endl;
+    cout<<"法一"<< cutRod(p, 3) <<endl;
     cout<<"法一"<< cutRod(p, 9) <<endl;
+
+    cout<<"\n法二"<< memoizedCutRod(p, 1) <<endl;
+    cout<<"法二"<< memoizedCutRod(p, 2) <<endl;
+    cout<<"法二"<< memoizedCutRod(p, 3) <<endl;
     cout<<"法二"<< memoizedCutRod(p, 9) <<endl;
+
+    cout<<"\n法三"<< bottomUpCutRod(p, 1) <<endl;
+    cout<<"法三"<< bottomUpCutRod(p, 2) <<endl;
+    cout<<"法三"<< bottomUpCutRod(p, 3) <<endl;
+    cout<<"法三"<< bottomUpCutRod(p, 9) <<endl;
     return 0;
 }
